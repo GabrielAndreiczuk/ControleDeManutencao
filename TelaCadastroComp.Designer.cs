b801cc9a904cfcc5c5f10415baad7a210988b1be
GabrielAndreiczuk <gabriel.andreiczuk@gmail.com>
2024-09-15 01:42:43 -0300

    Componentes Tela Complemento

--- TelaCadastroComp.Designer.cs
+++ TelaCadastroComp.Designer.cs
@@ -28,11 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cmbSetor = new System.Windows.Forms.ComboBox();
             this.rjButton1 = new Projeto_TCC.RoundedButton();
-            this.rjLabel1 = new Projeto_TCC.RoundedLabel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.roundedLabel1 = new Projeto_TCC.RoundedLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.roundedLabel2 = new Projeto_TCC.RoundedLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtContato = new System.Windows.Forms.TextBox();
+            this.roundedLabel3 = new Projeto_TCC.RoundedLabel();
+            this.cmbCargo = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // cmbSetor
+            // 
+            this.cmbSetor.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cmbSetor.BackColor = System.Drawing.Color.White;
+            this.cmbSetor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmbSetor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSetor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbSetor.FormattingEnabled = true;
+            this.cmbSetor.Location = new System.Drawing.Point(256, 217);
+            this.cmbSetor.Name = "cmbSetor";
+            this.cmbSetor.Size = new System.Drawing.Size(288, 27);
+            this.cmbSetor.TabIndex = 26;
             // 
             // rjButton1
             // 
@@ -42,39 +62,103 @@
             this.rjButton1.FlatAppearance.BorderSize = 0;
             this.rjButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.rjButton1.ForeColor = System.Drawing.Color.White;
-            this.rjButton1.Location = new System.Drawing.Point(250, 488);
+            this.rjButton1.Location = new System.Drawing.Point(250, 525);
             this.rjButton1.Name = "rjButton1";
             this.rjButton1.Size = new System.Drawing.Size(300, 50);
             this.rjButton1.TabIndex = 24;
-            this.rjButton1.Text = "Cadastrar-se";
+            this.rjButton1.Text = "Finalizar Cadastro";
             this.rjButton1.UseVisualStyleBackColor = false;
-            // 
-            // rjLabel1
-            // 
-            this.rjLabel1.BackColor = System.Drawing.Color.White;
-            this.rjLabel1.ForeColor = System.Drawing.Color.Black;
-            this.rjLabel1.Location = new System.Drawing.Point(250, 388);
-            this.rjLabel1.Name = "rjLabel1";
-            this.rjLabel1.Size = new System.Drawing.Size(300, 50);
-            this.rjLabel1.TabIndex = 25;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(260, 222);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(262, 27);
-            this.comboBox1.TabIndex = 26;
             // 
             // roundedLabel1
             // 
-            this.roundedLabel1.BackColor = System.Drawing.Color.Black;
+            this.roundedLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.roundedLabel1.BackColor = System.Drawing.Color.White;
             this.roundedLabel1.ForeColor = System.Drawing.Color.White;
-            this.roundedLabel1.Location = new System.Drawing.Point(250, 133);
+            this.roundedLabel1.Location = new System.Drawing.Point(250, 205);
             this.roundedLabel1.Name = "roundedLabel1";
             this.roundedLabel1.Size = new System.Drawing.Size(300, 50);
             this.roundedLabel1.TabIndex = 27;
             this.roundedLabel1.Text = "roundedLabel1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(257, 285);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 19);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Cargo";
+            // 
+            // roundedLabel2
+            // 
+            this.roundedLabel2.BackColor = System.Drawing.Color.White;
+            this.roundedLabel2.ForeColor = System.Drawing.Color.Black;
+            this.roundedLabel2.Location = new System.Drawing.Point(249, 305);
+            this.roundedLabel2.Name = "roundedLabel2";
+            this.roundedLabel2.Size = new System.Drawing.Size(300, 50);
+            this.roundedLabel2.TabIndex = 36;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(258, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 19);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Setor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(258, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 19);
+            this.label3.TabIndex = 38;
+            this.label3.Text = "Contato";
+            // 
+            // txtContato
+            // 
+            this.txtContato.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContato.Location = new System.Drawing.Point(260, 420);
+            this.txtContato.Name = "txtContato";
+            this.txtContato.Size = new System.Drawing.Size(286, 19);
+            this.txtContato.TabIndex = 39;
+            // 
+            // roundedLabel3
+            // 
+            this.roundedLabel3.BackColor = System.Drawing.Color.White;
+            this.roundedLabel3.ForeColor = System.Drawing.Color.Black;
+            this.roundedLabel3.Location = new System.Drawing.Point(250, 405);
+            this.roundedLabel3.Name = "roundedLabel3";
+            this.roundedLabel3.Size = new System.Drawing.Size(300, 50);
+            this.roundedLabel3.TabIndex = 40;
+            // 
+            // cmbCargo
+            // 
+            this.cmbCargo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cmbCargo.BackColor = System.Drawing.Color.White;
+            this.cmbCargo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmbCargo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCargo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbCargo.FormattingEnabled = true;
+            this.cmbCargo.Location = new System.Drawing.Point(256, 317);
+            this.cmbCargo.Name = "cmbCargo";
+            this.cmbCargo.Size = new System.Drawing.Size(288, 27);
+            this.cmbCargo.TabIndex = 41;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(234, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(332, 44);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Nome do Sistema";
             // 
             // TelaCadastroComp
             // 
@@ -82,10 +166,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(102)))));
             this.ClientSize = new System.Drawing.Size(800, 650);
-            this.Controls.Add(this.roundedLabel1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.rjLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbCargo);
+            this.Controls.Add(this.txtContato);
+            this.Controls.Add(this.roundedLabel3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.roundedLabel2);
+            this.Controls.Add(this.cmbSetor);
             this.Controls.Add(this.rjButton1);
+            this.Controls.Add(this.roundedLabel1);
             this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -95,14 +186,22 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private RoundedButton rjButton1;
-        private RoundedLabel rjLabel1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmbSetor;
         private RoundedLabel roundedLabel1;
+        private System.Windows.Forms.Label label1;
+        private RoundedLabel roundedLabel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtContato;
+        private RoundedLabel roundedLabel3;
+        private System.Windows.Forms.ComboBox cmbCargo;
+        private System.Windows.Forms.Label label4;
     }
 }
