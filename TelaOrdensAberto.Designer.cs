c78a58a19aa551a477a0d434a06ee93d0a70ac2e
GabrielAndreiczuk <gabriel.andreiczuk@gmail.com>
2024-10-09 21:55:21 -0300

    Inserção de itens dinamica

--- TelaOrdensAberto.Designer.cs
+++ TelaOrdensAberto.Designer.cs
@@ -28,15 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(102)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(984, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.AutoScroll = true;
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 100);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(984, 225);
+            this.panel3.TabIndex = 0;
             // 
             // TelaOrdensAberto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.ClientSize = new System.Drawing.Size(984, 550);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "TelaOrdensAberto";
             this.Text = "Form1";
@@ -45,5 +70,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
     }
 }
--- TelaOrdensAberto.cs
+++ TelaOrdensAberto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,7 +16,44 @@ namespace Projeto_TCC
         public TelaOrdensAberto()
         {
             InitializeComponent();
+            AdicionarComponentes();
         }
 
+        private void AdicionarComponentes()
+        {
+            string connectionString = "Server=localhost;Uid=root;Database=projeto;Port=3306";
+            List<string> ordens = new List<string>();
+            
+
+            using(MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string selectQuery = "SELECT ID_Abertura from abertura_ordem";
+
+                using (MySqlCommand command = new MySqlCommand(selectQuery, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ordens.Add(reader["ID_Abertura"].ToString());
+                        }
+                    }
+                }                                      
+            }
+            int yOffset = 10;
+
+            foreach (var id in ordens)
+            {
+                Label label = new Label()
+                {
+                    Text = $"ID: {id}",
+                    Location = new System.Drawing.Point(10, yOffset),
+                    AutoSize = true
+                };
+                this.panel3.Controls.Add(label);
+                yOffset += 30;
+            }
+        }
     }
 }
