6f02f494b9e3be0a086e29274b8858c55b294204
Gabriel Andreiczuk <gabriel.andreiczuk@gmail.com>
2024-11-25 17:31:32 -0300

    Geração de gráficos conforme dados do banco

--- TelaHome.cs
+++ TelaHome.cs
@@ -12,19 +12,78 @@ using LiveCharts.Wpf;
 using LiveCharts.Defaults;
 using LiveChartsCore;
 using System.Windows.Media;
+using MySql.Data.MySqlClient;
 
 namespace Projeto_TCC
 {
     public partial class TelaHome : Form
     {
+        string connectionString = "Server=localhost;Uid=root;Database=projeto;Port=3306";
+        int corretiva = 0, preditiva = 0, preventiva = 0;
         public TelaHome()
         {
             InitializeComponent();
+            CarregarDados();
         }
 
         private void TelaHome_Load(object sender, EventArgs e)
         {
             GerarGraficos();
+
+        }
+
+        private void CarregarDados() 
+        { 
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    int aberto = 0, concluido = 0;
+                    string selectQuery = "SELECT COUNT(*) FROM abertura_ordem WHERE Status = 1 OR Status = 2";
+
+                    using (MySqlCommand command = new MySqlCommand(selectQuery, connection))
+                    {
+                        aberto = Convert.ToInt32(command.ExecuteScalar());
+                        label3.Text = aberto.ToString();                                                
+                    }
+
+                    selectQuery = "SELECT COUNT(*) FROM abertura_ordem WHERE Status = 3";
+                    using (MySqlCommand command = new MySqlCommand(selectQuery, connection))
+                    {
+                        concluido = Convert.ToInt32(command.ExecuteScalar());
+                        label4.Text = concluido.ToString();
+                    }
+
+                    selectQuery = "SELECT (Tipo) FROM manutencao";
+                    using (MySqlCommand command = new MySqlCommand(selectQuery, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int selecao = int.Parse(reader["Tipo"].ToString());
+                                switch (selecao)
+                                {
+                                    case 1:
+                                        corretiva++;
+                                        break;
+                                    case 2:
+                                        preventiva++;
+                                        break;
+                                    case 3:
+                                        preditiva++;
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
         }
 
         private void GerarGraficos()
@@ -33,19 +92,19 @@ namespace Projeto_TCC
             {
                 new PieSeries {
                     Title = "Corretiva",
-                    Values = new ChartValues<double> {20},
+                    Values = new ChartValues<double> {corretiva},
                     DataLabels = true,
                     Fill = new SolidColorBrush(System.Windows.Media.Color.FromRgb(244, 67, 54))
                 },
                 new PieSeries {
                     Title = "Preventiva",
-                    Values = new ChartValues<double> {35},
+                    Values = new ChartValues<double> {preventiva},
                     DataLabels = true,
                     Fill = new SolidColorBrush(System.Windows.Media.Color.FromRgb(76, 175, 80))
                 },
                 new PieSeries {
                     Title = "Preditiva",
-                    Values = new ChartValues < double > { 45 }, 
+                    Values = new ChartValues < double > { preditiva }, 
                     DataLabels = true,
                     Fill = new SolidColorBrush(System.Windows.Media.Color.FromRgb(54, 124, 221))
                 },
